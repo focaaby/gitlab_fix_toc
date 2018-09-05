654dd1682e2ac3863cdb5f8d559cb698566e5872
Jerry Wang <focaaby@gmail.com>
2018-09-05 18:03:46 +0800

    :bug:Fixed toc ul and li level

--- main.js
+++ main.js
@@ -35,28 +35,31 @@ function checkFileReady() {
     if (typeof(fileContent) == "undefined") {
         window.setTimeout(checkFileReady, 100);
     } else {
-        toc = "<div class='gitlab_toc ui-widget-content'>";
-        toc += "<ul class='gitlab_toc_ul'>";
+        var toc = document.createElement('div');
+        toc.className = 'gitlab_toc ui-widget-content';
+        var toc_ul = document.createElement('ul');
+        toc_ul.className = 'nav gitlab_toc_ul';
+        toc.appendChild(toc_ul);
+        var $toc_ul = $(toc_ul);
+
         prev_level = 1;
         for(i = 0; i < fileContent.childNodes.length; i++) {
             if(["H1", "H2", "H3", "H4", "H5"].includes(fileContent.childNodes[i].nodeName)) {
                 level = fileContent.childNodes[i].nodeName[1];
                 if (level > prev_level) {
-                    toc += "<ul class='gitlab_toc_ul'>";
+                    $toc_ul = $toc_ul.children().last('li');
+                    $toc_ul.append(`<ul class='nav'>`)
+                    $toc_ul = $toc_ul.children().last('ul');
                 } else if (level < prev_level) {
-                    for(j = 0; j < prev_level - level; j++) {
-                        toc += "</ul>";
-                    }
+                    $toc_ul = $toc_ul.parent().parent();
                 }
                 
                 href = fileContent.childNodes[i].childNodes[1].getAttribute("href");
-                toc += "<li><a href=\"" + href + "\">" + fileContent.childNodes[i].innerText + "</a></li>";
+                $toc_ul.append(`<li id="toc-${i}"><a href="${href}">${fileContent.childNodes[i].innerText}</a></li>`);
                 prev_level = level;
             }
         }
-        toc += "</ul></div>";
         $("body").append($(toc));
-
         $(".gitlab_toc").resizable({handles: "all"});
         $(".gitlab_toc").draggable();
     }
